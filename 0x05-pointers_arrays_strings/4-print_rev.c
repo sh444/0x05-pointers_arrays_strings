6503dd51a52355a90a4390dbe48caa7d7a488cec
sh444 <souhaila.arsanne@gmail.com>
2023-02-28 15:26:04 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int  j, len;
+
+	
+	len = int _strlen(s);;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
