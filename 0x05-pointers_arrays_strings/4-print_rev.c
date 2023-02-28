9f03fcececa3de948bf40fa47126ad1f1f93938c
sh444 <souhaila.arsanne@gmail.com>
2023-02-28 15:40:14 -0800

    last

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,21 @@
  */
 void print_rev(char *s)
 {
-	int  j, len;
-	char *str;
-	str = s;
-	
-	len = _strlen(str);
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
 
 	for (j = len - 1; j >= 0; j--)
 	{
 		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
