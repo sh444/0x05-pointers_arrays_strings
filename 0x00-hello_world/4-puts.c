77be113827329176ae9bfbff217b0151eb093582
sh444 <souhaila.arsanne@gmail.com>
2023-03-06 04:50:04 -0800

    commit 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Write a C program that prints exactly "Programming is like building a multilingual puzzle"
- *  followed by a new line
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
