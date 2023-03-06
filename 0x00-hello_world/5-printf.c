dc97dadff80014f9a56268c7dc9aa9ec4f3736b3
sh444 <souhaila.arsanne@gmail.com>
2023-03-06 04:40:34 -0800

    commit 2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints exactly with proper grammar
+ * but the outcome is a piece of art
+ * Return: 0 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
